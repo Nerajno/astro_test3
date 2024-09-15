3ee627c53dc5ff3b8b89d16429c1389a679a75d9
Nerando Johnson <nerajno@gmail.com>
2024-09-15 09:30:35 -0400

    fixing email - updating vercel config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from './remark-reading-time.mjs';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-portfolio-v3-dusky.vercel.app",
   output: "hybrid",
+  adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap()],
   image: {
     domains: ["picsum.photos"],
