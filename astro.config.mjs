f3902a11e489547429195a6ba0fe1bf31d979369
Nerando Johnson <nerajno@gmail.com>
2024-11-01 06:50:12 -0400

    Fix attempt for the astro configure - till its fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,6 +30,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/static';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 const OUTPUT_MODES = {
