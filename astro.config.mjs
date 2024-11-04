39a785bf9554d91f49e8bf5282b0ff4a8727f3d2
Nerando Johnson <nerajno@gmail.com>
2024-11-04 16:50:52 -0500

    fixing config file - again with Nick v4 - static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 export default defineConfig({
   site: "https://astro-portfolio-v3-dusky.vercel.app",
-  output: 'server',
-  adapter: vercel({
-    imageService: true,
-  }),
+  output: 'static',
+  adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap()],
   image: {
     domains: ["picsum.photos"],
