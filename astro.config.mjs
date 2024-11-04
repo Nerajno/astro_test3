7b8c2df78117a9583eb9d34a3f352e69ee7d05d7
Nerando Johnson <nerajno@gmail.com>
2024-11-04 16:42:35 -0500

    fixing config file - again with Nick v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/serverless';
-import node from '@astrojs/node';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 export default defineConfig({
   site: "https://astro-portfolio-v3-dusky.vercel.app",
-  output: "server",
+  output: 'server',
   adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap()],
   image: {
