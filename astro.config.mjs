3bf0fb73756aeae23730221462e409d3bcd65a24
Nerando Johnson <nerajno@gmail.com>
2024-11-06 20:07:21 -0500

    trying for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,21 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from './remark-reading-time.mjs';
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // Conditionally import the Vercel adapter
-let vercelAdapter;
-if (process.env.NODE_ENV === 'production' || process.env.VERCEL) {
-  vercelAdapter = () => import('@astrojs/vercel/serverless');
-}
+// let vercelAdapter;
+// if (process.env.NODE_ENV === 'production' || process.env.VERCEL) {
+//   vercelAdapter = () => import('@astrojs/vercel/serverless');
+// }
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-portfolio-v3-dusky.vercel.app",
   output:  "server",
   // adapter: process.env.NODE_ENV === 'production' ? vercelAdapter() : undefined,
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [tailwind(), mdx(), sitemap()],
   image: {
     domains: ["picsum.photos"],
