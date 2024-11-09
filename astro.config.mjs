be83b6da9fe34624df3f6570bb7bb970fbe1d211
Nerando Johnson <nerajno@gmail.com>
2024-11-09 06:05:10 -0500

    Updating  for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from './remark-reading-time.mjs';
-// import vercel from "@astrojs/vercel/serverless";
 import netlify from "@astrojs/netlify";
 
-// Conditionally import the Vercel adapter
-// let vercelAdapter;
-// if (process.env.NODE_ENV === 'production' || process.env.VERCEL) {
-//   vercelAdapter = () => import('@astrojs/vercel/serverless');
-// }
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astro-portfolio-v3-dusky.vercel.app",
   output:  "server",
-  // adapter: process.env.NODE_ENV === 'production' ? vercelAdapter() : undefined,
   adapter: netlify(),
   integrations: [tailwind(), mdx(), sitemap()],
   image: {
