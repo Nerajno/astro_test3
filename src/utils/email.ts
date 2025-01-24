3f504fa28d7441d5034e3f08ce5f42edf7803ed6
Nerando Johnson <nerajno@gmail.com>
2025-01-24 12:15:07 -0500

    Kept working on the contact form - nodemailer  ~ fixed it

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -11,6 +11,7 @@ async function sendEmail(props: ISendEmail) {
   let transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.EMAIL_PASS,
